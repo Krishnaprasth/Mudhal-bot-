d0f88fa2982b5951e68c06d9064a3602e166fb6d
Krishnaprasth <ca.krishnaprasath@gmail.com>
2025-06-23 13:56:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-csv_data = """Month,Store,Metric,Amount
-Apr 24,EGL,Gross Sales,1443916.0
-Apr 24,EGL,Net Sales,1382617.82
-Apr 24,EGL,COGS (food +packaging),541505.3072999845
-Apr 24,EGL,Gross margin,841112.5127000156
-Apr 24,EGL,store Labor Cost,262392.3408
-Apr 24,EGL,Utility Cost,83000.0
-Apr 24,EGL,CAM,0.0
-Apr 24,EGL,Aggregator commission,0.0
-Apr 24,EGL,Marketing & advertisement,21471.89
-Apr 24,EGL,Other opex expenses,128807.075
-Apr 24,ITPL,Gross Sales,6681978.0
-Apr 24,ITPL,Net Sales,6565784.7
-Apr 24,ITPL,COGS (food +packaging),2287132.58
-Apr 24,ITPL,Gross margin,4278652.12
-Apr 24,ITPL,store Labor Cost,877823.6528
-Apr 24,ITPL,Utility Cost,141657.66
-Apr 24,ITPL,CAM,0.0
-Apr 24,ITPL,Aggregator commission,153491.86
-Apr 24,ITPL,Marketing & advertisement,105453.5
-Apr 24,ITPL,Other opex expenses,466223.195
-Dec 24,EGL,Gross Sales,1530000.0
-Dec 24,EGL,Net Sales,1450000.0
-Dec 24,EGL,COGS (food +packaging),550000.0
-Dec 24,EGL,Gross margin,900000.0
-Dec 24,EGL,store Labor Cost,275000.0
-Dec 24,EGL,Utility Cost,85000.0
-Dec 24,EGL,CAM,5000.0
-Dec 24,EGL,Aggregator commission,20000.0
-Dec 24,EGL,Marketing & advertisement,25000.0
-Dec 24,EGL,Other opex expenses,135000.0
-..."""
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -39,6 +6,7 @@ from io import StringIO
 
 @st.cache_data
 def load_data():
+    csv_data = open("cleaned_store_data_embedded.csv", "r").read()
     return pd.read_csv(StringIO(csv_data))
 
 df_raw = load_data()
